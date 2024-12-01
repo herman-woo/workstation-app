c99025f5b43abd548eec7c1b8da66035ecb43b85
Herman <hermanwoo100@gmail.com>
2024-12-01 18:15:19 -0500

    Flattened Page folder, list components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
-import { AccountComponent } from './routes/account/account.component';
-import { DashboardComponent } from './routes/dashboard/dashboard.component';
-import { UnderwriterRecordComponent } from './routes/underwriter-record/underwriter-record.component';
-import { UnderwriterListComponent } from './routes/underwriter-list/underwriter-list.component';
-import { NamedInsuredsListComponent } from './routes/named-insureds-list/named-insureds-list.component';
-import { NamedInsuredRecordComponent } from './routes/named-insured-record/named-insured-record.component';
-import { BrokerListComponent } from './routes/broker-list/broker-list.component';
-import { BrokerRecordComponent } from './routes/broker-record/broker-record.component';
-import { BrokerCompaniesListComponent } from './routes/broker-companies-list/broker-companies-list.component';
-import { RiskSummaryTabComponent } from './routes/account/account-tabs/risk-summary-tab/risk-summary-tab.component';
-import { CompanyRecordComponent } from './routes/company-record/company-record.component';
+import { AccountComponent } from './pages/account/account.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UnderwriterRecordComponent } from './pages/underwriter-record/underwriter-record.component';
+import { UnderwriterListComponent } from './pages/underwriter-list/underwriter-list.component';
+import { NamedInsuredsListComponent } from './pages/named-insureds-list/named-insureds-list.component';
+import { NamedInsuredRecordComponent } from './pages/named-insured-record/named-insured-record.component';
+import { BrokerListComponent } from './pages/broker-list/broker-list.component';
+import { BrokerRecordComponent } from './pages/broker-record/broker-record.component';
+import { BrokerCompaniesListComponent } from './pages/broker-companies-list/broker-companies-list.component';
+import { RiskSummaryTabComponent } from './pages/account/account-tabs/risk-summary-tab/risk-summary-tab.component';
+import { CompanyRecordComponent } from './pages/company-record/company-record.component';
 
 export const routes: Routes = [
     //main page references
