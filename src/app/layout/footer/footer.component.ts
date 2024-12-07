df96235181f794bbeb1accf9cbef17d5494f296c
Tina Pham <tina96pham@gmail.com>
2024-12-07 15:05:01 -0500

    Debug footer version

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import * as data from "../../../../package.json"
+import packageJson from "../../../../package.json"
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 
@@ -16,8 +16,12 @@ import { RouterLink, RouterModule } from '@angular/router';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  version = data.version
-  currentDate: Date = new Date()
-  day = this.currentDate.toDateString()
+  version: string = packageJson.version;
+  currentDate: Date = new Date();
+  day: string = this.currentDate.toDateString();
+  constructor() {
+    console.log("package.json:", packageJson); // Log the entire package.json
+    console.log("App Version:", this.version);  // Log the version
+  }
 
 }
